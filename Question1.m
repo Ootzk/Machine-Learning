0b9e6c678099f37ccadbcb4156f3a49c237943fc
Namho Kwon <blitz555@dgist.ac.kr>
2019-03-24 23:32:44 +0900

    loaded again...!

---
+++ Question1.m
@@ -0,0 +1,14 @@
+% set figure option
+figure(1), ylim([-1.5, 1.5]), title('Question 1'), xlabel('x'), ylabel('y'), hold on;
+
+% plot original signal y = sin(2 * pi * x)
+plot(linspace(0, 1), sin(2 * pi * linspace(0, 1)), 'g'), hold on;
+
+% generate gaussian noise samples with range = [0, 1], number of samples = 10
+gaussianNoise_samples_10 = GaussianNoise_generator([0, 1], 10);
+
+% plot gaussian noise samples
+plot(gaussianNoise_samples_10.x, gaussianNoise_samples_10.y, 'o'), hold on;
+
+% save variables for Question2.m
+save('gaussianNoise10.mat', 'gaussianNoise_samples_10');
---
+++ Question4.m
@@ -0,0 +1,5 @@
+quadratic_regularization_order = [1, 3, 5, 9];
+
+for order = quadratic_regularization_order
+    x = quadprog();
+end
